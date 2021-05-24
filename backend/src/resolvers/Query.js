b05ab1153d1b932bc8b168d7bd6007b33908e807
Zora Santos <zoranildo22@gmail.com>
2021-05-24 20:09:03 -0300

    Cria consulta de lançamentos

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const { getUserId } = require('./../utils')
 
 function user (_, args, ctx, info) {
@@ -42,8 +43,46 @@ function categories (_, { operation }, ctx, info) {
   }, info)
 }
 
+function records (_, {month, type, accountsIds, categoriesIds }, ctx, info) {
+  const userId = getUserId(ctx)
+  let AND = [ { user: { id: userId } } ]
+  AND = !type ? AND : [...AND, { type }]
+
+  AND = !accountsIds || accountsIds.length === 0
+    ? AND
+    : [
+      ...AND,
+      { OR:  accountsIds.map(id => ({ account: { id } }))}
+    ]
+
+  AND = !categoriesIds || categoriesIds.length === 0
+    ? AND
+    : [
+      ...AND,
+      { OR:  categoriesIds.map(id => ({ category: { id } }))}
+    ]
+
+    if (month) {
+      const date = moment(month, 'MM-YYYY')
+      const startDate = date.startOf('month').toISOString()
+      const endDate = date.endOf('month').toISOString()
+
+      AND = [
+        ...AND,
+        { date_gte: startDate },
+        { date_lte: endDate }
+      ]
+    }
+
+    return ctx.db.query.records({
+      where: { AND },
+      orderBy: 'date_ASC'
+    }, info)
+}
+
 module.exports = {
   user,
   accounts,
-  categories
+  categories,
+  records
 }
